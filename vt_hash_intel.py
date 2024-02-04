5cc419fb85e6fe8d4692bccb2edbd951f3428afd
Mohamed Abdelrahman <60690190+Mohamed-Abdelrahman-7@users.noreply.github.com>
2024-02-04 21:32:34 +0200

    Add files via upload
    this script for getting Virus total file hash reputation on file hash/es (comma separated)

---
+++ vt_hash_intel.py
@@ -0,0 +1,28 @@
+import vt,json
+def hash_intel_vt(client,hash):
+    """take a vt.Client object and a filehash "string" and returns the last_analysis_stats for that hash"""
+    parameters={'query':hash}
+    filehashReport=""
+    try:
+        hashReport=client.iterator("/search",params=parameters)
+        for i in hashReport:
+            stats=i.get("last_analysis_stats")
+            filehashReport+="VirusTotal filehash analysis: filehash is reported as harmless by ({}), type unsupported by ({}), suspicious by ({}),  malicious by ({}) and undected by ({})\n"\
+                .format(*[stats.get(key) for key in ["harmless","type-unsupported","suspicious","malicious","undetected"]])
+        if len(filehashReport)==0:
+            filehashReport="VirusTotal doesn't have information available for that filehash !!\n"
+    except Exception as e:
+        filehashReport="an error {} occured while trying to query VT for the filehash !!".format(e)
+    return filehashReport 
+
+# replace your vt api key with your key and leave the quotes you will find your virus total api key by clicking on your profile and going to API key
+VT_api_key="your vt api key"
+
+vtClient=vt.Client(VT_api_key)
+
+hashes=[x.strip() for x in (input("Enter file hash or multiple file hashes with comma separated: ")).split(',')]
+
+print('\n')
+for filehash in hashes:
+    print(filehash+":\n"+hash_intel_vt(vtClient,filehash)+'\n')
+    
